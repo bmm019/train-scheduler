e396a3fe6bf0fce17573e6e11894eb08bd39cc2c
Brittany McNulty <Brittany.m.mcnulty@gmail.com>
2019-06-13 18:59:57 -0500

    added more js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
     appId: "1:7147069919:web:3af2812b07c2fc9f"
   };
   
-  firebaseConfig.initialize(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 
   // Assign the reference to the database to a variable named 'database'
 // var database = ...
@@ -32,5 +32,49 @@ $(document).ready(function() {
 	updateTime();
 	setInterval(updateTime, 1000);
 });
+
+// button for adding trains
+$("#add-train-btn").on("click", function(event) {
+	event.preventDefault();
+
+	// grabs user input
+	var trainName = $("#train-name-input").val().trim();
+	var trainDestination = $("#destination-input").val().trim();
+	var trainTime = $("#train-time-input").val().trim();
+    var trainFrequency = parseInt($("#frequency-input").val().trim());
+    
+    if (trainName === "" || trainDestination === "" || trainName === "" || trainFrequency === "" ){
+        return;
+    }
+
+	// local temporary object for storing train data
+	var newTrain = {
+		name: trainName,
+		destination: trainDestination,
+		time: trainTime,
+		frequency: trainFrequency
+	};
+
+	// uploads the train data to the firebase database
+	database.ref().push(newTrain);
+
+	console.log(newTrain.name);
+	console.log(newTrain.destination);
+	console.log(newTrain.time);
+	console.log(newTrain.frequency);
+
+	
+
+	// clears all of the text boxes
+	$("#train-name-input").val("");
+	$("#destination-input").val("");
+	$("#train-time-input").val("");
+	$("#frequency-input").val("");
+
+	// prevents loading a new page
+	return false;
+});
+
+
 
 
